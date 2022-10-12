d26ce9255440023ef27972ad00000ef3dcf1a92c
misaeljlo91 <misaeljlo91@gmail.com>
2022-10-12 13:37:09 -0300

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
     </div>
   );
 }
